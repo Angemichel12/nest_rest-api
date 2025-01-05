d794621fc6661d957e555836707811d20abca2b7
Angemichel12 <angemicheluwizeye@gmail.com>
2025-01-05 14:54:52 +0200

    add provider

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,28 +6,30 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return this.usersService.findAll(role);
   }
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id };
+  findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
   }
   @Post()
-  create(@Body() user: {}) {
-    return user;
-  }
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatedUser: {}) {
-    return { id, ...updatedUser };
-  }
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id };
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersService.create(user);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'ENGINEER',
+    },
+  ];
+  findAll(role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    if (role) {
+      return this.users.filter((user) => user.role == role);
+    }
+    return this.users;
+  }
+  findOne(id: number) {
+    console.log('+++++++++++', id);
+    return this.users.find((user) => (user.id = id));
+  }
+  create(user: {
+    name: string;
+    email: string;
+    role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+  }) {
+    const userByHighestId = this.users.sort((a, b) => b.id - a.id);
+    const newuser = { id: userByHighestId[0].id + 1, ...user };
+    this.users.push(newuser);
+    return newuser;
+  }
+}
